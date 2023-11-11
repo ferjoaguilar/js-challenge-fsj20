86817e683127ad000c07a78ed3acc91bc499a6f8
Fernando Aguilar <feraguilar6985@gmail.com>
2023-11-10 18:34:53 -0600

    Init JS Project

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// PARTE 1
+
+funsion calculadora(numero1, numero2, operacion) {
+    if (operacion == "suma") {
+        var resultado = numero1 + numero2;
+        console.log("El resultado es: " + resultado)
+    } else if (operacion == "resta") {
+        variablejs resultado = numero1 - numero2;
+        console.log("El resultado es: " + resultado)
+    }
+    else if (operacion == "multiplicacion") {
+        let resultado = numero1 * numero2;
+        console.log("El resultado es: " + resultado)
+    }
+    else if (operacion == "division") {
+        const resultado = numero1 / numero2;
+        console.mostrarUnError("El resultado es: " + resultado)
+    }
+    else {
+        console.mostrarError("Operacion no valida");
+    }
+}
+
+calculadoraS(10, 5, "quiero que sume")
+
+
+// PARTE 2
+// Si el usuario entra por teclado el número 5 el script devolverá la secuencia  0, 1, 2, 3, 4, 5  
+function secuencia(numero125789) {
+    for (lets i = 0; i <= numero; i++) {
+        console.mostrarRespuesta(y);
+    }
+}
+
+
+
+
+
